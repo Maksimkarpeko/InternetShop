b9e98b9b57a8eed7a8dc5f178c9ee73cbd8de3be
Maksim <maksim_karpe2ko@mail.ru>
2025-11-23 23:36:29 +0300

    feat:created header

--- src/modules/Header/Header.tsx
+++ src/modules/Header/Header.tsx
@@ -1,14 +1,32 @@
-import logo from "assets/logo.svg";
-
+import basket from "assets/basket.svg";
+import search from "assets/search.png";
 export const Header = () => {
   return (
-    <header className=" flex gap-4 bg-gray-300">
-      <div className="ml-5 mt-3">
-        <img src={logo} alt="Logo" width={"50px"} />
-      </div>
+    <header className="flex flex-wrap justify-around bg-bg h-21 ">
       <div className="mt-2">
-        <h1 className="text-2xl font-bold">General Store</h1>
-
+        <h1 className="text-5xl text-second-color font-bold">General Store</h1>
+      </div>
+      <div className="mt-4 relative">
+        <img src={search} alt="search" className="absolute top-3 left-2" />
+        <input
+          type="text"
+          name="text"
+          className="outline-none bg-second-color text-gray-100 text-2xl rounded-xl p-1 pl-8.5 h-12"
+        />
+      </div>
+      <div className="w-[11%] mt-5">
+        <button
+          className=" w-full h-11 bg-second-color pr-3 rounded-xl text-white text-xl font-bold cursor-pointer flex justify-center   shadow-md transform active:scale-[0.95] duration-300"
+          type="button"
+        >
+          <div className="pr-4 border-r border-r-white/50 h-7 mt-1.5">
+            520 $
+          </div>
+          <div className="w-10 flex pl-3 ">
+            <img src={basket} alt="basket" width={"25px"} className="" />
+            <span className="pt-1.5 pl-2">3</span>
+          </div>
+        </button>
       </div>
     </header>
   );
